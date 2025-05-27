9a9865268e083f4025c8cf0c4e95d3b724a47a57
mammumammi <ashinmammu@gmail.com>
2025-05-27 16:33:05 +0530

    or youw chang
    Lines starting
    updated about.jsx

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -31,16 +31,23 @@ const About = () => {
     if (width < 768){
     
       animateWithGsapTimeline(tl,'.content',{opacity:1,duration:10},{start:'35% 80%',end:'50% 80%',scrub:0.6}) 
+      
     }
 
     let split=SplitText.create(".content",{
       type:"chars"
     })
+    let splitedu=SplitText.create(".edu",{
+      type:"lines"
+    })
+    
+    gsap.set(split.chars, { display: "inline" })
+    gsap.set(split.lines, { display: "inline" })
     if (width<768){
     gsap.to(split.chars,{
-      color:"#EABDE6",
-      scale:1.2,
-      y:-5,
+      fontSize:"16px",
+      color:"#e1b7ff",
+      
       ease:"power4.in",
       duration:5,
       stagger:0.5,
@@ -52,8 +59,62 @@ const About = () => {
       
         pin:true,
       }
+
+      
     })
+    gsap.to(splitedu.lines,{
+      color:"#f1e3fc",
+      
+      
+      ease:"power4.in",
+      duration:5,
+      stagger:0.5,
+      scrollTrigger:{
+        trigger:'.edu',
+        start:'top 70%',
+        end:"100% 70%",
+        scrub:0.7,
+      
+        
+      }
+    })
+    
+      animateWithGsapTimeline(tl,'.eduhead',{y:-20,opacity:1,duration:2},{start:'20% 80%',end:'45% 80%',scrub:0.9,})
+      animateWithGsapTimeline(tl,'.edu',{y:-20,opacity:1,duration:2},{start:'20% 80%',end:'45% 80%',scrub:0.9,})
+    
+    
   }
+  if (width>768){
+    gsap.to(split.chars,{
+      color:"#e1b7ff",
+      
+      ease:"power4.in",
+      duration:5,
+      stagger:0.03,
+      scrollTrigger:{
+        trigger:'.content',
+        start:'top 80%',
+        end:"bottom 70%",
+        scrub:0.7,
+      }
+  })
+  gsap.to(splitedu.lines,{
+    color:"#f1e3fc",
+    
+    
+    ease:"power4.in",
+    duration:5,
+    stagger:0.25,
+    scrollTrigger:{
+      trigger:'.edu',
+      start:'top 80%',
+      end:"100% 70%",
+      scrub:0.7,
+    
+      
+    }
+  })
+}
 
   })
  
@@ -76,11 +137,11 @@ const About = () => {
         <div className='md:px-2  md:flex md:flex-1/2 md:-mt-2 flex-col md:flex-row ml-5 md:ml-0 mr-5 '> 
 
         
-        <div className={`content normText text-1xl md:mr-5 md:w-[40vw] ${width <768 ? 'opacity-0' :''}  mt-9 md:mt-0`}   >Diving into the world of coding from 2021,From my 12th standard of starting Python to creating responsive websites on React.<br/>
+        <div className={`content normText text-[15px] md:text-xl md:mr-5 md:w-[40vw] ${width <768 ? 'opacity-0' :''}  mt-9 md:mt-0 pb-15 font-black `}  style={{wordBreak: 'normal', overflowWrap: 'break-word' }} >Diving into the world of coding from 2021,From my 12th standard of starting Python to creating responsive websites on React.<br/>
         <br/>Just A Guy aiming to be Ever-learning.◡̈<br/><br/>I love to create websites that gives immersive experience to the users.I love to learn new technologies and kill myself in the process.<br/><br/>Love to Get my brains plugged out into finding that one semicolumn i missed.<br/><br/>Looking to get myself on Blockchain Technology and Artificial Intelligence because Why not.<br/><br/>Has a thing to create stuffs that i visualise on my mind especially websites!!.
         </div>
-        {width < 768 ? <div className='me text-5xl flex items-center justify-center my-12'>Education</div> : null}
-        <div className=' normText text-1xl md:mr-5 edu'  >B.Tech Computer Science And Engineering<br />
+        {width < 768 ? <div className={`me text-5xl  eduhead flex items-center justify-center my-12  ${width<768? 'opacity-0':''}`}>Education</div> : null}
+        <div className={`normText text-[15px] md:text-xl   md:mr-5 edu ${width<768? 'opacity-0':''}`}  >B.Tech Computer Science And Engineering<br />
         Cochin University Of Science And Technology,Kochi <br />2023-2027<br />CGPA:9.05<br/>Semester 4<br/><br/>Alan Feldman Public School,Thiruvananthapuram<br/>2020<br/>High School Education<br/>93.4%<br/><br/>St Antony's Public School,Kanjirapally,Kottayam<br/>2021-2022<br/>Higher Secondary School Education<br/>89.4%
         </div>
         </div>
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     })
   return (
     <div className=' mt-2 py-2 flex justify-center items-center fixed z-50 bgcolor left-1/2 transform -translate-x-1/2 navbg px-5 nav rounded-3xl text-md'>
-        <nav className='flex md:space-x-18 space-x-5  sd:text-5'> 
+        <nav className='flex md:space-x-18 space-x-5  text-[15px] md:text-[17px]'> 
             <div className=''>About</div>
             <div>Projects</div>
             <div>Resume</div>
