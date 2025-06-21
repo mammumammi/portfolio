d6cdf1b65de7ebed9953eb7b687b82edc263753c
mammumammi <ashinmammu@gmail.com>
2025-06-21 23:07:05 +0530

    ef

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,14 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server : {
+    host:true,
+    
+  },
   
-  build: {
-    rollupOptions: {
-      external: ['@gsap/react']
-    }
-},
-server : {
-  host:true,
-  
-},
 })
