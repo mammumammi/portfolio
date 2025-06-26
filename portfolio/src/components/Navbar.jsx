167bf1a793be62a0b270498be9abe356ef87621b
mammumammi <ashinmammu@gmail.com>
2025-06-26 11:05:30 +0530

    again changes on navbar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -16,7 +16,12 @@ const Navbar = () => {
     })
     })
   return (
-    <div className=' mt-2 py-2 flex justify-center items-center fixed z-50 bgcolor left-1/2 transform -translate-x-1/2 navbg px-5 nav rounded-3xl text-md'>
+    <div className=' mt-2 py-2 flex justify-center items-center fixed z-50 bgcolor left-1/2 transform -translate-x-1/2 navbg px-5 nav rounded-3xl text-md' style={{
+        backgroundColor: "rgba(24,19,53,0.25)",
+        backdropFilter: "blur(10px)",
+        WebkitBackdropFilter: "blur(10px)",
+        border: "0.5px solid #190e2d"
+      }} >
         <nav className='flex md:space-x-18 space-x-5  text-[15px] md:text-[17px]'> 
             <div className=''>About</div>
             <div>Projects</div>
