05115aa355aaddca670c4191addf591d66ffd84a
mammumammi <ashinmammu@gmail.com>
2025-07-04 15:13:22 +0530

    Made changes to Project section

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import web from '../assets/weblink.svg'
-import apple from '../assets/apple.jpg';
+import apple from '../assets/apple.svg';
 import brainwave from '../assets/brainwave-symbol.svg';
 const Projects = () => {
 
@@ -57,14 +57,14 @@ const Projects = () => {
 ];
 
   return (
-    <div className='h-auto w-screen'>
+    <div className='h-full w-screen '>
         <div className='me md:text-8xl text-6xl flex items-center justify-center py-[50px]'>Projects</div>
-        <div className='relative '>
+        <div className='relative  h-[200vh] md:h-auto '>
             
         
-            <video src="https://res.cloudinary.com/ddbkg48oy/video/upload/so_0,eo_33/v1750918004/Screen_Recording_2025-06-26_at_11.35.26_AM_w10fcv.mov" autoPlay muted playsInline loop className='w-full items-center rounded-[100px] brightness-50' />
+            <video src="https://res.cloudinary.com/ddbkg48oy/video/upload/so_0,eo_33/v1750918004/Screen_Recording_2025-06-26_at_11.35.26_AM_w10fcv.mov" autoPlay muted playsInline loop className='w-full items-center rounded-[100px] brightness-50 ' />
             
-            <div className='absolute top-[8%]  flex items-center'>
+            <div className='absolute top-[8%]  flex items-center '>
               <div className=' flex items-center flex-col gap-y-5 normText '>
                 <p className=' md:text-4xl text-3xl '>Iphone 15 Pro Website</p>
                 <p className='w-[60dvw] text-center'>A mock website that features the Iphone 15 Pro and Iphone 15 Pro Max.A high perfomance website that features snappy and modern animations </p>
@@ -82,18 +82,21 @@ const Projects = () => {
 
 
 
-            <div className='md:w-full mx-auto text-white absolute top-[50%] z-20 md:left-[8%] overflow-hidden' >
-              <div className='flex flex-wrap gap-4  overflow-hidden '>
+            <div className='md:w-full  text-white absolute md:top-[65%] md:left-0 top-[40%] left-[10vw] z-20  md:overflow-hidden' >
+              <div className='md:w-[70vw]  md:mx-auto  overflow-x-auto gap-6 px-4 w-[80vw] mx-auto md:h-auto h-[100vh] overflow-y-auto md:overflow-y-none whitespace-nowrap md:space-x-8 space-y-8 scrollbar-hide '>
 
               
-              {projList.map((item,index) => (
-                <div key={index} className='md:w-[25vw] md:h-[20vh] box hover:blur-[2px] overflow-hidden relative transiion duration-300 rounded-[25px] bg-black' >
+              {[...projList].map((item,index) => (
+                <>
+                <div key={index} className='md:w-[400px]  md:inline-block cursor-pointer md:h-[200px] h-[200px] box  overflow-hidden relative transiion  rounded-[25px] bg-black group' >
+                <p className='absolute text-center w-full top-[4%] normText text-[16px] opacity-0 group-hover:opacity-100 group-hover:blur-none z-10 font-[1000] duration-300 '>{item.title}</p>
                   
                      
-                <img src={item.logo} alt="" className='w-full h-full object-contain object-center' />
+                <img src={item.logo} alt="" className='w-[8vw] flex m-auto items-center justify-center group-hover:blur-[2px] duration-300 relative h-full ' />
             
-         
+                
         </div>
+        </>
               ))}
                 </div>
             </div>
