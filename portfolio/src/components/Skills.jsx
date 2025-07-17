f90e7d5f554546f369bfc43023c47852c1ac5d76
mammumammi <ashinmammu@gmail.com>
2025-07-17 22:27:44 +0530

    new

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -121,7 +121,7 @@ const Skills = () => {
 
     const tmain = gsap.timeline();
     width > 768 ?  animateWithGsapTimeline(tmain,'.skillsection',{ y:-50,opacity:1,duration:2,stagger:0.5},{start:'30% 80%',end:'50% 80%',scrub:0.6}) :
-    animateWithGsapTimeline(tmain,'.skillsection',{y:-20,opacity:1,duration:2},{start:'top 90%',end:'center center',scrub:0.9,markers:true})
+    animateWithGsapTimeline(tmain,'.skillsection',{y:-20,opacity:1,duration:2},{start:'top 90%',end:'45% 80%',scrub:0.9})
     
 
     gsap.to('.mainrowmain',{
@@ -137,8 +137,8 @@ const Skills = () => {
 
     const tl1= gsap.timeline({scrollTrigger:{
       trigger:'maintitle',
-      start:'top 60%',
-      end:'bottom top',
+      start:'60% 15%',
+      end:'80% 20%',
       toggleActions:'play play reverse play',
       
       
